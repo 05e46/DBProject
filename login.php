5ee948bb292fa20d65bae2970ecf3b848ec5c16b
Felipe Perez <phillip.perez44@gmail.com>
2017-04-28 19:55:11 -0700

    simple changes

--- login.php
+++ login.php
@@ -1,4 +1,18 @@
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Register</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="flex-container">
+
 <form action="loginConfirm.php" , method="POST"></form>
 <h3>Please enter the following fields</h3>
 <label for="Username" class="sr-only">Username</label>
@@ -11,4 +25,7 @@
 <br>
 <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
 
+    </form>
+    </div>
+    </body>
 </html>
