6d2a0db0a5967e79f37556c51a66810ee7837779
RJSkywalker91 <rmerino19926@gmail.com>
2017-05-01 17:42:26 -0700

    added code to confirm login. BUT it is under my personal username and database. Change it to your own in the code please

--- login.php
+++ login.php
@@ -1,31 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Register</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="stylesheet/styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-<div class="flex-container">
-
-<form action="loginConfirm.php" , method="POST"></form>
-<h3>Please enter the following fields</h3>
-<label for="Username" class="sr-only">Username</label>
-<input type="text" name="userName" clas="form-control" placeholder="User Name" required>
-<br>
-<br>
-<label for="Password" class="sr-only">Password</label>
-<input type="password" name="passWord" class="form-control" placeholder="Password" required>
-<br>
-<br>
-<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-
-    </form>
-    </div>
-    </body>
-</html>
+<?php
+  if (!isset($_POST)){
+    echo "<p>No data was sent.<br>Please try again.</p>";
+    exit;
+  }
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $db = new mysqli('127.0.0.1','phpAdmin','password','practice'); #(ip address, username, password, database)
+  if(!$db){
+    echo "Error connecting to database.";
+    exit;
+  }
+  $stmt = $db->prepare("SELECT Username, Password FROM Users WHERE Username = ? AND Password = ?");
+  $stmt->bind_param("ss", $username, $password);
+  $stmt->execute();
+  $stmt->store_result();
+  if ($stmt->num_rows == 0) {
+    $stmt->close();
+    $db->close();
+    echo "<p>There are no users matching this username and password.<br>Please try again</p>";
+    echo "<form action='index.html'><input type='submit' value='Go Back'>";
+    exit;
+  }
+  else {
+    $stmt->close();
+    $db->close();
+    session_start();
+    $_SESSION['user'] = $username;
+    $_SESSION['login'] = $password;
+    header("Location: dashboard.php");
+  }
+?>
