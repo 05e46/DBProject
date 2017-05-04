dfd7b2ccf1a2113ae25c87353dc23fd9ad2306c5
Felipe Perez <phillip.perez44@gmail.com>
2017-05-03 20:11:46 -0700

    chat setup

---
+++ chatroom.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Chats</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>       
+    </head>
+    
+    <body>
+       
+       <!-- top bar -->
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="dashboard.php">DBProject</a> </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Threads</a></li>
+                    <li><a href="chatroom.php">Chatrooms</a></li>
+                </ul>
+                
+                <ul class="nav navbar-nav navbar-right">
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type = "text" class="form-control" placeholder="Search ChatRoom"></div>
+                        <button type="submit" class="btn btn-default">Search for chatroom</button>
+                    </form>
+                </ul>
+            </div>
+        </div>
+        </nav>
+                    
+                    
+                    
+        
+    </body>
+    
+    
+    
+</html>
