d8d8c8c6bbb6d74b814ccb46d8467edb3b37e990
RJSkywalker91 <rmerino19926@gmail.com>
2017-05-03 20:21:52 -0700

    Small changes to login, redid dashboard.

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php
+  session_start();
+  $db = new mysqli('127.0.0.1','phpAdmin','password','practice'); #(ip address, username, password, database)
+  if(!$db){
+    echo "Error connecting to database.";
+    exit;
+  }
+?>
 <head>
     <title>Home page</title>
     <meta charset="utf-8">
@@ -20,7 +27,7 @@
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
                     <li><a href="#">Dashboard</a></li>
-                    <li><a href="#">Threads</a></li>
+                    <li><a href="#">Forums</a></li>
                     <li><a href="#">Chatrooms</a></li>
 
                     <!-- <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tags<span class="caret"></span></a>
@@ -42,32 +49,29 @@
                 </ul>
             </div>
         </div>
-    </nav>  
-    <!-- end top bar -->
-    <!-- When the posts begin -->
-    <div class="flex-container">
-        <article class="posts">
-            <h1>First post</h1>
-            <p>Please use the database to have the text pop here</p>
-            <p>Please note that there are other things that should be in this page</p>
-            <p><strong>Get this shit done!!!</strong></p>
-        </article>
-    </div>
-    <div class="flex-container">
-        <article class="posts">
-            <h1>Second post</h1>
-            <p>Please use the database to have the text pop here</p>
-            <p>Please note that there are other things that should be in this page</p>
-            <p><strong>Get this shit done!!!</strong></p>
-        </article>
-    </div>
-    <div class="flex-container">
-        <article class="posts">
-            <h1>Third post</h1>
-            <p>Please use the database to have the text pop here</p>
-            <p>Please note that there are other things that should be in this page</p>
-            <p><strong>Get this shit done!!!</strong></p>
-        </article>
+    </nav>
+
+    <!--Mailbox-->
+    <div class="flex-container" id="mailbox">
+      <div class="col-sm-8" id="title">
+          <?php
+          $stmt = $db->prepare("SELECT Subject, Status, Sender, Receiver FROM Mailbox WHERE Receiver = ?");
+          $stmt->bind_param("s", $_SESSION['user']);
+          $stmt->execute();
+          $stmt->bind_result($subject, $status, $sender, $receiver);
+
+          while ($stmt->fetch() == TRUE) {
+                  echo '<div class="row">
+                  <button type="button" class="btn">
+                  <div>Subject: ></div><div id="subject">' + $subject + '</div>
+                  <div>From: </div><div id="sender">' + $sender + '</div>
+                  </button></div>';
+          }
+          $stmt->close();
+          ?>
+      </div>
+      <div class="col-sm-4" id="message">
+      </div>
     </div>
     <!-- end of posts -->
 </body>
