b35ee389188d4ee70fbd1908f23a9a0d911a2162
RJSkywalker91 <rmerino19926@gmail.com>
2017-05-10 12:07:39 -0700

    Started the userPosts.php file

--- userPosts.php
+++ userPosts.php
@@ -1,6 +1,24 @@
 <?php
-include('header.php'); 
+include('header.php');
 ?>
-
-//This will be where users can post to the thread
-
+<body>
+  <div class="flex-container">
+    <div class="pull-left">
+      <a id="backToThread" href="/Thread.php">Back to Threads</a>
+    <h1>Posts</h1>
+  </div>
+    <?php
+      $stmt = $db->prepare("SELECT postText, Poster FROM Post WHERE ThreadNo = 2 ORDER BY PostNo");
+      $stmt->execute();
+      $stmt->bind_result($text, $poster);
+      while ($stmt->fetch() == TRUE) {
+        echo '
+        <div class="post">
+        <div class="poster">'.$poster.'</div>
+        <div class="postText">'.$text.'</div>
+        </div>
+        ';
+      }
+      $stmt->close();
+    ?>
+</body>
