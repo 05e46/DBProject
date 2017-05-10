c4e9306ce2664d23a4da0bc3934a23afcd44a07c
RJSkywalker91 <rmerino19926@gmail.com>
2017-05-10 10:19:26 -0700

    Made it so the user connects to the database in the header. Now all  calls will execute correctly when called from a file that contains header.php

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+  $db = new mysqli('127.0.0.1','phpAdmin','password','practice'); #(ip address, username, password, database)
+  if(!$db){
+    echo "Error connecting to database.";
+    exit;
+  }
+?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -5,33 +13,35 @@
     <link rel="stylesheet" href="stylesheet/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="dashboard.php">DBProject</a> </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li><a href="/dashboard.php">Dashboard</a></li>
-                <li><a href="/forum.php">Forums</a></li>
-                <li><a href="#">Chatrooms</a></li>
-
-                <!-- <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tags<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">#yes</a></li>
-                            <li><a href="#">#No</a></li>
-                            <li><a href="#">#what</a></li>
-                            <li><a href="#">more...</a></li>
-                        </ul> -->
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <form class="navbar-form navbar-left">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search"> </div>
-                    <button type="submit" class="btn btn-default">Search</button>
-                </form>
-            </ul>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> <a class="navbar-brand" href="login_register.html">DBProject</a> </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <?php
+                      if (basename($_SERVER['PHP_SELF']) != "dashboard.php") {
+                        echo '<li><a id= "dashboardButton" href="/dashboard.php">Dashboard</a></li>';
+                      }
+                      if (basename($_SERVER['PHP_SELF']) != "forum.php") {
+                        echo '<li><a id="forumButton" href="/forum.php">Forums</a></li>';
+                      }
+                    ?>
+                    <li><a id="chatButton" href="#">Chatrooms</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search"> </div>
+                        <button type="submit" class="btn btn-default">Search</button>
+                        <?php
+                          if ($_SESSION["user"]){
+                            echo '<button id="logout">Logout</button>';
+                          }
+                        ?>
+                    </form>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</head>
