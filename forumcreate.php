f239e1fd5bd082cb97651dbb4130fe12554283de
RJSkywalker91 <rmerino19926@gmail.com>
2017-05-15 13:21:38 -0700

    redid changes on forumcreate.php

--- forumcreate.php
+++ forumcreate.php
@@ -1,34 +1,61 @@
 <?php session_start();
-$con = mysqli_connect('localhost', 'root', '', 'dbproject');
-if(! $con)
-{
-die('Connection Failed'.mysql_error());
+
+//$db = new mysqli('ecsmysql','cs431s25','ixupeijo','cs431s25');
+$db = new mysqli('127.0.0.1', 'phpAdmin', 'password', 'practice');
+//$db = new mysqli('127.0.0.1','root','','dbproject'); #(ip address, username, password, database)
+if(!$db){
+  echo "Error connecting to database.";
+  exit;
 }
-//mysql_select_db(dbpro,$con);
-
-$user = $_SESSION['user'];
-
+//Make sure a form has been submitted
 if(isset($_REQUEST['submit'])!='')
 {
-    If($_REQUEST['forumName']=='' || $_REQUEST['description']=='')
+    //Make sure both of the fields are not empty
+    if($_REQUEST['forumName']=='' || $_REQUEST['description']=='')
     {
-        Echo "please fill the empty field.";
+        Echo "Please fill the empty field.";
     }
     else
     {
-        $sql="INSERT INTO forum(forumName,description,StartModerator)
-        VALUES('".$_REQUEST['forumName']."', '".$_REQUEST['description']."', '$user')";
-        $res=mysqli_query($con,$sql);
-        if($res)
-        {
-            header("Location: forum.php");
-            $sql->close();
-            $con->close();
+      //Instantly creates a forum that can be viewed if the user is an admin
+      if ($_REQUEST['user'] == "admin") {
+        $status = "open";
+        $stmt = $db->prepare("INSERT INTO FORUM (forumName, description, StartModerator) VALUES (?,?,?,?)");
+        $stmt->bind_param("ssss", $_REQUEST['forumName'], $_REQUEST['description'], $_REQUEST['user'], $status);
+        if ($stmt->execute()){
+          $stmt->close();
+          $db->close();
+          header("Location: forum.php");
         }
-        else
-        {
+        else {
           echo "Something went wrong, can not add the forum";
         }
+      }
+
+
+      //Instantly creates a forum that must be approved by an admin before being viewed
+      if ($_REQUEST['user'] == "moderator"){
+        $status = "request";
+        $stmt = $db->prepare("INSERT INTO FORUM (forumName, description, StartModerator) VALUES (?,?,?,?)");
+        $stmt->bind_param("ssss", $_REQUEST['forumName'], $_REQUEST['description'], $_REQUEST['user'], $status);
+        if ($stmt->execute()){
+          $stmt->close();
+          $db->close();
+          header("Location: forum.php");
+          alert("Forum awaiting approval from admin");
+        }
+        else {
+          echo "Something went wrong, can not add the forum";
+        }
+      }
+
+      
+      //Something went wrong
+      else {
+        header("Location: forum.php");
+        alert("Something has gone wrong. Unable to create forum");
+
+      }
     }
 }
 
